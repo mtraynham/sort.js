ed11ec04b66a97abfdb197172e24243ecd98d016
Matt Traynham <skitch920@gmail.com>
2016-03-31 08:33:52 -0400

    Updates deps

--- dist/sort.js
+++ dist/sort.js
@@ -1,5 +1,5 @@
 /*!
- *  sort.js - v0.0.1 - Fri Mar 25 2016 13:21:09 GMT-0400 (EDT)
+ *  sort.js - v0.0.1 - Thu Mar 31 2016 08:31:43 GMT-0400 (EDT)
  *  https://github.com/mtraynham/sort.js.git
  *  Copyright 2015-2016 Matt Traynham <skitch920@gmail.com>
  *
